54e95516f8cd6d3aa4e96370f3cac128597f30c9
Zarah Dominguez <zarah@zdominguez.com>
2018-09-04 15:21:48 +1000

    Initial commit

---
+++ app/src/main/java/demo/zdominguez/com/disabledbuttondemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package demo.zdominguez.com.disabledbuttondemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
